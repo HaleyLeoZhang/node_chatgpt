fe8c0bd283f2b56e94a65483bbed3dca90d89b7e
沐临风 <hlzblog@vip.qq.com>
2023-02-14 18:01:58 +0800

    update

--- es6/services/Translate/ChatgptService.js
+++ es6/services/Translate/ChatgptService.js
@@ -1,6 +1,6 @@
 import Base from './Base'
 import Log from "../../tools/Log";
-import {ChatGPTAPI} from 'chatgpt'
+import {ChatGPTAPI} from 'chatgpt' // 文档 https://github.com/transitive-bullshit/chatgpt-api
 import {AI} from "../../conf";
 
 export default class ChatgptService extends Base {
@@ -14,12 +14,11 @@ export default class ChatgptService extends Base {
             debug: AI.debug,
         })
 
-        let raw_text = `中文转英文，翻译以下内容：   ${chinese_text}`
+        let raw_text = `只翻译以下内容，要求中文转英文：   ${chinese_text}`
         // raw_text = 'Hello World!' // TODO 用于测试
         const res = await api.sendMessage(raw_text, {
             timeoutMs: AI.timeout_second * 1000,
         })
-        console.log(res.text)
-        Log.ctxInfo(ctx, "结束")
+        return res.text
     }
 }
