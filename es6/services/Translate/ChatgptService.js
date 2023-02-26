42abdeaaf484a29c9be171903628ae2117de60ea
娌愪复椋? <hlzblog@vip.qq.com>
2023-02-26 12:52:31 +0800

    update

--- es6/services/Translate/ChatgptService.js
+++ es6/services/Translate/ChatgptService.js
@@ -8,35 +8,37 @@ import ConversationCache from "../../caches/ConversationCache";
 export default class ChatgptService extends Base {
     // 初始化API对象
     static new_api_object() {
-        const api = new ChatGPTAPI({
-            apiKey: AI.open_ai_api_key,
-            debug: AI.debug,
-        })
-        return api
+        if (this.open_ai === undefined) {
+            this.open_ai = new ChatGPTAPI({
+                apiKey: AI.open_ai_api_key,
+                debug: AI.debug,
+            })
+        }
+        return this.open_ai
     }
 
     // 初始bing API对象，可能有限流 ---
     static new_bin_api_object() {
-        // 申请权限 https://www.xiaoheiwoo.com/how-to-apply-new-bing-and-free-to-use-chatgpt/
-        // 要加入后补名单并审核通过后  翻墙访问 https://www.bing.com/new
-        // 要用户登录的cookie TODO cookie 可能要放到 redis 里， 方便修改 --- 目前 cookie 过期时间不确定
-        const cookiesRaw = `
- _EDGE_V=1; MUID=133D590A73B7662A06334B3D72656787; MUIDB=133D590A73B7662A06334B3D72656787; USRLOC=HS=1; SRCHD=AF=ANAB01; SRCHUID=V=2&GUID=828024AF806B4FB290D4C62C41A7961F&dmnchg=1; MUIDV=NU=1; MMCASM=ID=710FEE1D631143DFBD7B4A402D524B04; ANIMIA=FRE=1; _tarLang=default=zh-Hans; _TTSS_IN=hist=WyJlbiIsImF1dG8tZGV0ZWN0Il0=; _TTSS_OUT=hist=WyJ6aC1IYW5zIl0=; ZHCHATSTRONGATTRACT=TRUE; SRCHS=PC=EDGEDBB; _SS=PC=EDGEDBB&SID=3425F75B79E76E861264E59878C46FE6&R=200&RB=0&GB=0&RG=200&RP=200; CSRFCookie=07066984-bef2-4399-91c7-30780291ba31; ANON=A=7395C27F5C8C28B66130CA77FFFFFFFF&E=1c07&W=1; NAP=V=1.9&E=1bad&C=XzZM_KtVzQFMFZqfb982maVMRMezEVV39cGrhug7BhWwew4I70vE6g&W=1; PPLState=1; KievRPSSecAuth=FABqBBRaTOJILtFsMkpLVWSG6AN6C/svRwNmAAAEgAAACMRL8FXBP+hDKASZQzFAU15dIqf7tFRGNOd+atDrhvwaQ+67SKxKeXnulAz44G2pgtcVCDZ/1lBYuRKDZUYtJibH2ipNcI49fTNgXlmXXLXx7hUUW90RsVbEBuGdvPWTwqaesVqgHzdKLBVsV78MGh1/0d5syN7Q9p5Clnfs2K6YcgaLI7SPRdBfm5xtwtrceXbqzNcN4CM36FI4iodegDkPMNGk3Pd6E+4YO4VfOdPa0ev8K8bKNsil7bQffeRpZeCASNudbwWBtuihPqbYiWdPzKEhi6FSZRQVbiFVZx2o2LJhgRf7wR7j5WzEfVqGqPejwC2P0Dyr2jDU1ycqbQKjNWS5rJp2cydKvESfpUWH9krntHFNLH9wi/Ob2g6lZHBcnbubtwR7jwQcPBef2K2RvqC9I7qv0JctV0CdKTOtLe30WPtiJmbLI0nRKIVIEMI7m3BftNkrNghzwt7p2qNzAGhowB2pG/HT+9dZCGAI9BSQ73wYdBHMz6mzWXdn1viWY0/NzBxJgsOeyYdAEST9ISu1BylNRH2XOdbxpfnB5DinjEDtdEUtV3kXQKeCPedO8fsRPSk2SMCcnGAt8AMMm71GiUtUQ97AWPL2k8byluIo+nil8F/XDYub0r/UX5aQ6NXUh9BxtOtVEhbcOlKDSXw+559pAF7A/POYUveLrTT8YWQ5tGaTnGCGXeMK8BolJgagQ1P5L9MWlS2FfaABzYNo8Cl8zPo4WO8P4LgSxAnrj1G2+ospM10Yg9UUlSmGj+aQFyf0gXYZX0Q7qWuJ6hAv8aPZ90MelH6hO3SmDcKW+QXz3pe9SSldC2xB1ece/xSnXgaBePYIhZstQvU7M2h3H44S+CO4VGJLYHZn+rN+IxSctDDWWZVOPse4wzQGriv0qacwViK53zKBI/ikb74ix40GneN4DKK4M48OanFvK+IeQU/JHP1zMcKdpxWVHhvqnRJov4h5kuPssMDoaerSWXUC9tyCV9QdVZh6IwqGEy3IERjdZaofBpYNw859KKfEYBeD/5hbh0BhxUrLKAIBRBNMdmW5WdsgxQCouAkcpg1Mt1T/1a1i39ezA0ZCCx5hN+ChImmzeK2E3eSTwhU7Zmlw078MRCJ6wEiBJMOL2RHfWdIro+NNwceQlQfxvYTG8lk+g+8bKrBJUcTJt2uw/qrDfhLnUiGv9/pYgcYoKBliM/9mL7JFdfAqfcnFT2hqv1AdysJc0ABsEQM/krvnFzBzbjElm6hiSWCtIHh27pQMRCr/9dMY7ZiDR7KWRUmVwbkoaQ9Bh8Iggfrzr2E0VWQOfCcAVtAISRMUHS1AHyY9KNKgMCOVylTkzBPRWOnGoILz5HJydHLJpqCojp+T81kxIHqbF7FCMdNsW3Bt4lgclQwdXnP5tAaywVZWaEBfZTLlomROQS6XBuN65xQAV6u2RHqPjgeEvY6id+HmbEW8TX4=; WLS=C=78c517683574e1ac&N=%e4%b8%b4%e9%a3%8e; WLID=GVB06Wy9vB36aU99hqh07L96XgFtMTSMAGHMm/xMq3ifOsxMGxrwJgghz8LVXQtWEaHSGv3DoSmrSKyfqdSyX9EQhJg55MVGY6GtE9Q03RQ=; SUID=A; ZHCHATWEAKATTRACT=TRUE; ZHSEARCHCHATSTATUS=STATUS=1; ZHLASTACTIVECHAT=0; SRCHUSR=DOB=20221008&T=1677348102000&POEX=W; ipv6=hit=1677351704949&t=4; _EDGE_S=SID=3B5FF3C45E6D69EA3A2AE1075FB06873&mkt=zh-cn; SNRHOP=I=&TS=; _U=1q2cKcbq3sNksT2gZgCfVoaDcPe7SHW7HDB27KO-24OVIWmet0Rjl9p36R_Upuuz0HbDDLlEcWu-ggASsr5MRallth4TYjx_RbhtPqJsx8nO5DmiWJPsLyefDOaayUgA4xgDOILGj8287jf3_llS14Unn6NIpJ3JJdDK-oy1qHUa5FxGxZ6zcHohyxGjhjxYY3Sg6nJYx7QQtoPgyeME-Zt1Zys3AdXsQHQa5UuEAyAE; SRCHHPGUSR=BRW=W&BRH=T&CW=1352&CH=1042&SCW=1335&SCH=3664&DPR=1.3&UTC=480&DM=0&SRCHLANG=zh-Hans&PV=10.0.0&PRVCW=1352&PRVCH=1042&EXLTT=31&HV=1677349110&WTS=63812941179&BZA=0&VCW=2031&VCH=1042&PR=1.25; ABDEF=V=13&ABDV=11&MRNB=1677349111283&MRB=0; _RwBf=ilt=294&ihpd=0&ispd=12&rc=200&rb=0&gb=0&rg=200&pc=200&mtu=0&rbb=0&g=0&cid=&clo=0&v=12&l=2023-02-25T08:00:00.0000000Z&lft=2023-02-02T00:00:00.0000000-08:00&aof=0&o=2&p=&c=&t=0&s=0001-01-01T00:00:00.0000000+00:00&ts=2023-02-25T18:18:32.2706234+00:00&rwred=0&wls=&lka=0&lkt=0&TH=&W=1
+        if (this.bing_ai === undefined) {
+            // 申请权限 https://www.xiaoheiwoo.com/how-to-apply-new-bing-and-free-to-use-chatgpt/
+            // 要加入后补名单并审核通过后  翻墙访问 https://www.bing.com/new
+            // 要用户登录的cookie TODO cookie 可能要放到 redis 里， 方便修改 --- 目前 cookie 过期时间不确定
+            const cookiesRaw = `_EDGE_V=1; MUID=133D590A73B7662A06334B3D72656787; MUIDB=133D590A73B7662A06334B3D72656787; USRLOC=HS=1; SRCHD=AF=ANAB01; SRCHUID=V=2&GUID=828024AF806B4FB290D4C62C41A7961F&dmnchg=1; MUIDV=NU=1; MMCASM=ID=710FEE1D631143DFBD7B4A402D524B04; ANIMIA=FRE=1; _tarLang=default=zh-Hans; _TTSS_IN=hist=WyJlbiIsImF1dG8tZGV0ZWN0Il0=; _TTSS_OUT=hist=WyJ6aC1IYW5zIl0=; ZHCHATSTRONGATTRACT=TRUE; ANON=A=7395C27F5C8C28B66130CA77FFFFFFFF&E=1c07&W=1; WLID=GVB06Wy9vB36aU99hqh07L96XgFtMTSMAGHMm/xMq3ifOsxMGxrwJgghz8LVXQtWEaHSGv3DoSmrSKyfqdSyX9EQhJg55MVGY6GtE9Q03RQ=; SUID=A; ZHCHATWEAKATTRACT=TRUE; ZHLASTACTIVECHAT=0; ABDEF=V=13&ABDV=11&MRNB=1677349143678&MRB=0; _UR=QS=0&TQS=0; _clck=qb7smp|1|f9f|0; _clsk=2rpfo6|1677349841222|18|0|f.clarity.ms/collect; _EDGE_S=SID=0C5D9420074069200FED86E3066D6884; WLS=C=78c517683574e1ac&N=%e4%b8%b4%e9%a3%8e; _U=1dmHoFKR219VuVh87qGxFBAd7R5peFCd3cPLr0zQD7AVpQ_aEzUyg_bbbJ25vSv56PevdP0dJeaVnzqVF0yOWTiitS105TOH19rdBWg2asYdIu5Y61bh1SeXeo_LJ0eyYHYnCfY_ae0HSWSxbe9qSRouXATpGUv9yPvPEkh1dooPa-eIvcPIalN1ZfcO8C5BTpE-E0Un8X5Aa6KGxF7-mdQBoYRebFJRzrQx1mXPe93U; SRCHS=PC=EE24; MicrosoftApplicationsTelemetryDeviceId=b8def018-2378-4334-a53d-417c876684ba; _HPVN=CS=eyJQbiI6eyJDbiI6MSwiU3QiOjAsIlFzIjowLCJQcm9kIjoiUCJ9LCJTYyI6eyJDbiI6MSwiU3QiOjAsIlFzIjowLCJQcm9kIjoiSCJ9LCJReiI6eyJDbiI6MSwiU3QiOjAsIlFzIjowLCJQcm9kIjoiVCJ9LCJBcCI6dHJ1ZSwiTXV0ZSI6dHJ1ZSwiTGFkIjoiMjAyMy0wMi0yNVQwMDowMDowMFoiLCJJb3RkIjowLCJHd2IiOjAsIkRmdCI6bnVsbCwiTXZzIjowLCJGbHQiOjAsIkltcCI6Nn0=; SRCHUSR=DOB=20221008&T=1677386044000&POEX=W; ipv6=hit=1677389646801&t=4; _RwBf=ilt=300&ihpd=2&ispd=15&rc=12&rb=12&gb=0&rg=0&pc=12&mtu=0&rbb=0.0&g=0&cid=&clo=0&v=46&l=2023-02-25T08:00:00.0000000Z&lft=0001-01-01T00:00:00.0000000&aof=0&o=0&p=bingcopilotwaitlist&c=MY00IA&t=7412&s=2023-02-25T18:22:06.6959696+00:00&ts=2023-02-26T04:34:24.0799609+00:00&rwred=0&wls=1&lka=0&lkt=0&TH=&W=1&mta=0&e=lR2sJ7NGKCX-9p0WmitHvrYqa4QUJ4Ngv5lG7YVUeDJFZ7veMFpMrDjFnrA4csu5yc6nngKsa9zJuZLraZsc9Q&A=; _SS=SID=0C5D9420074069200FED86E3066D6884&PC=EE24&R=12&RB=12&GB=0&RG=0&RP=12; SRCHHPGUSR=BRW=W&BRH=T&CW=1352&CH=1042&SCW=1402&SCH=1844&DPR=1.3&UTC=480&DM=0&SRCHLANG=zh-Hans&PV=10.0.0&PRVCW=1352&PRVCH=1042&EXLTT=31&HV=1677386064&WTS=63812941179&BZA=0&VCW=2031&VCH=1042&PR=1.25`
 
-        `
-        const api = new BingAIClient({
-            // Necessary for some people in different countries, e.g. China (https://cn.bing.com)
-            host: 'https://cn.bing.com',
-            // "_U" cookie from bing.com
-            userToken: '',
-            // If the above doesn't work, provide all your cookies as a string instead
-            cookies: cookiesRaw,
-            // A proxy string like "http://<ip>:<port>"
-            // proxy: 'http://127.0.0.1:7890',
-            // (Optional) Set to true to enable `console.debug()` logging
-            debug: AI.debug,
-        });
-        return api
+            this.bing_ai = new BingAIClient({
+                // Necessary for some people in different countries, e.g. China (https://cn.bing.com)
+                host: 'https://cn.bing.com',
+                // "_U" cookie from bing.com
+                userToken: '',
+                // If the above doesn't work, provide all your cookies as a string instead
+                cookies: cookiesRaw,
+                // A proxy string like "http://<ip>:<port>"
+                // proxy: 'http://127.0.0.1:7890',
+                // (Optional) Set to true to enable `console.debug()` logging
+                debug: AI.debug,
+            });
+        }
+        return this.bing_ai
     }
 
     /**
